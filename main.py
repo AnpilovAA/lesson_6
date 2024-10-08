b2953d892986cca8d3de6a0aa14d561826d793c6
Anpilov Andrey <a.anpilov93@gmail.com>
2024-10-08 17:40:25 +0300

    close spet 5

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 
+def password_symbol(password: str):
+    for char in password:
+        if char.isdigit():
+            print(char, "-", "Цифра")
+        else:
+            print(char, "-", "Буква")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     password = input("Введите пароль: ")
+
     if len(password) < 12:
         print("Короткий")
+        password_symbol(password=password)
     else:
+        password_symbol(password=password)
         print("Длинный")
