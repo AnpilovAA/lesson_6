4721c8736bc83ccb35c3b2ea366fa459c3d2b261
Anpilov Andrey <a.anpilov93@gmail.com>
2024-10-10 17:29:10 +0300

    step 7

--- main.py
+++ main.py
@@ -10,12 +10,25 @@ def password_symbol(password: str):
         print("Нет цифр")
 
 
+def is_very_long(password: str) -> bool:
+    if len(password) < 12:
+        return False
+    else:
+        return True
+
+
+def has_digit(password: str) -> bool:
+    digit = False
+    for char in password:
+        if char.isdigit():
+            digit = True
+    if digit:
+        return digit
+    else:
+        return digit
+
+
 if __name__ == "__main__":
     password = input("Введите пароль: ")
-
-    if len(password) < 12:
-        print("Короткий")
-        password_symbol(password=password)
-    else:
-        password_symbol(password=password)
-        print("Длинный")
+    print(is_very_long(password=password))
+    print(has_digit(password=password))
