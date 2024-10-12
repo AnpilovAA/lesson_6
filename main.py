1e4d38a05ceab9f4b2c79d8a6a267d8a3f16aca5
Anpilov Andrey <62002015+AnpilovAA@users.noreply.github.com>
2024-10-12 11:01:38 +0300

    step 13

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-
-def password_symbol(password: str):
-    digit = False
-    for sign in password:
-        if sign.isdigit():
-            digit = True
-    if digit:
-        print("Есть цифра")
-    else:
-        print("Нет цифр")
-
-
 def is_very_long(password: str) -> bool:
     if len(password) < 12:
         return False
@@ -22,26 +10,80 @@ def has_digit(password: str) -> bool:
     for sign in password:
         if sign.isdigit():
             digit = True
+            break
     if digit:
         return digit
     else:
         return digit
 
 
-def password_rating(password: str):
+def has_letters(password: str) -> bool:
+    letter = False
+    for sign in password:
+        if sign.isalpha():
+            letter = True
+            break
+    if letter:
+        return letter
+    else:
+        return letter
+
+
+def has_upper_letters(password: str) -> bool:
+    upper_letter = False
+    for sing in password:
+        if sing.isupper():
+            upper_letter = True
+            break
+    if upper_letter:
+        return upper_letter
+    else:
+        return upper_letter
+
+
+def has_lower_letters(password: str) -> bool:
+    lower_letter = False
+    for sing in password:
+        if sing.islower():
+            lower_letter = True
+            break
+    if lower_letter:
+        return lower_letter
+    else:
+        return lower_letter
+
+def has_symbols(password: str) -> bool:
+    symbol = False
+    for sign in password:
+        if not sign.isdigit() and not sign.isalpha():
+            symbol = True
+            break
+    if symbol:
+        return symbol
+    else:
+        return symbol
+
+
+def password_rating(password: str) -> str:
     scope = 0
 
-    how_long_pass = is_very_long(password=password)
-    has_digit_in_pass = has_digit(password=password)
-    if has_digit_in_pass:
-        scope += 2
-    if how_long_pass:
-        scope += 2
-    return scope
+    check_conditions = [
+        is_very_long(password=password),
+        has_digit(password=password),
+        has_letters(password=password),
+        has_upper_letters(password=password),
+        has_lower_letters(password=password),
+        has_symbols(password=password)
+    ]
+
+    for check_result in check_conditions:
+        if check_result:
+            scope += 2
+    
+    return f"Рейтинг пароля: {scope}"
 
 
 if __name__ == "__main__":
-    password = input("Введите пароль: ")
-    # print(is_very_long(password=password))
-    # print(has_digit(password=password))
+    # password = input("Введите пароль: ")
+    password = "12dasc!gfW9112"
     print(password_rating(password=password))
